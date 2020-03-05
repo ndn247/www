ebd8deaa24bd89713230618368b1091f06bdb3cc
Nam Nguyen <nampdnguyen@gmail.com>
2020-03-04 22:03:20 -0600

    added javascript and finished popup

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('Hey there!')
+
+function showPopup() {
+	var popup = document.getElementById('popup');
+	popup.classList.add('show');                               // adds code from class 'show' from CSS to element popup in HTML
+}
+
+function hidePopup() {
+	var popup = document.getElementById('popup');
+	popup.classList.remove('show');
+}
